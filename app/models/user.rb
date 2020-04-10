9f619e45a4608e9ee9374ff1ade7ec730fb5db8a
github username <github email address>
2020-04-10 17:29:45 -0600

    added bcrypt gem and 'has_secure_password' to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :research_goals
   has_many :resources, through: :research_goals
+  has_secure_password #access to authenticate and password confirmation
 end
