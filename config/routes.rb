27b3dd239007488af8c1097d4bcd23d7c4a54876
github username <github email address>
2020-04-10 17:37:51 -0600

    added routes for resources, including a signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  get '/signup' => 'users#new'
+
+  resources :users 
+  resources :research_goals 
+  resources :resources 
+  resources :journals 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
