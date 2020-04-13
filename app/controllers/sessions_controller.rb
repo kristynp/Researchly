c9f3c7ae36c8a27a95bf7ea1ce1b29ef2c9cae7b
github username <github email address>
2020-04-12 21:04:33 -0600

    added redirects and errors to session create action. Added current_user, logged_in?, and redirect_if_not_logged_in helpers, added flash message if login was not successful

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry 
+    binding.pry
+    user = User.find_by(user_name: params[:user][:user_name])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "Incorrect login info. Please try again."
+      redirect_to "/login"
+    end
   end
 end 
