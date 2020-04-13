3f30b4ebb8c18741d518da76971530a13f2b57e2
github username <github email address>
2020-04-13 12:39:38 -0600

    added new research goal form, new and create actions in the research goals controller, strong params for research goal

--- app/controllers/research_goals_controller.rb
+++ app/controllers/research_goals_controller.rb
@@ -1,2 +1,17 @@
 class ResearchGoalsController < ApplicationController 
+  
+  def new 
+    @research_goal = ResearchGoal.new 
+  end
+
+  def create
+    binding.pry 
+  end
+
+  private 
+
+  def research_goal_params
+    params.require(:research_goal).permit(:name, :description) 
+  end
+
 end
