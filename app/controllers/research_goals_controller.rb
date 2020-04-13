08b11710350d775d1368f9b828c9837d49ae207d
github username <github email address>
2020-04-13 11:56:10 -0600

    added links in application controller for navigation to research goal index and new pages. Added research goal controller, and the above two view files.

---
+++ app/controllers/research_goals_controller.rb
@@ -0,0 +1,2 @@
+class ResearchGoalsController < ApplicationController 
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
     user = User.find_by(user_name: params[:user][:user_name])
     if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
