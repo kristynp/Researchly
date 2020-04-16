7b797d77262b3ecba1e7c777b18906fba4d63d90
github username <github email address>
2020-04-16 10:22:30 -0600

    modified form to create resource from show page to use research goal and resource, modified create action for resources.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -3,6 +3,7 @@ class ResourcesController < ApplicationController
 
   def index
     if params[:research_goal_id] && @research_goal = ResearchGoal.find_by_id(params[:research_goal_id]) 
+      #if it's nested and we find the post  
       @resources = @research_goal.resources
     else
       @error = "That research goal does not exist" if params[:research_goal_id]
@@ -10,12 +11,20 @@ class ResourcesController < ApplicationController
     end
   end
 
-  def new 
-    @resource = Resource.new 
+  def new
+ 
+    if params[:research_goal_id] && @research_goal = ResearchGoal.find_by_id(params[:research_goal_id]) 
+      @research_goal = ResearchGoal.find_by_id(params[:research_goal_id])  
+      @resource = @research_goal.resources.build(resource_params)
+    else
+      @error = "That research goal does not exist" if params[:research_goal_id]
+      @resource = Resource.new
+    end 
+
   end
 
   def create
-    @resource = current_user.research_goal.resources.build(resource_params)
+    @resource = current_user.research_goals.resources.build(resource_params)
     if @resource.save
       redirect_to resources_path 
     end 
