aa642e4432a46d7a7b02a80e61af215d497a7130
github username <kristyparker1@gmail.com>
2020-08-15 22:35:04 -0600

    debug new action in research goal notes controller

--- app/controllers/research_goal_notes_controller.rb
+++ app/controllers/research_goal_notes_controller.rb
@@ -1,3 +1,25 @@
 class ResearchGoalNotesController < ApplicationController
   before_action :redirect_if_not_logged_in
+
+  def new
+    if params[:research_goal_id] && @research_goal = ResearchGoal.find_by_id(params[:research_goal_id]) 
+      @research_goal = ResearchGoal.find_by_id(params[:research_goal_id]) 
+      #binding.pry
+      @research_goal_note = @research_goal.research_goal_notes.build
+    else
+      @error = "That research goal does not exist" if params[:research_goal_id]
+      @research_goal_note = ResearchGoalNote.new
+    end 
+  end
+
+  def create 
+
+  end
+
+  private 
+  
+  def research_goal_note_params
+    params.require(:research_goal_note).permit(:title, :content, :research_goal_id) 
+  end
+
 end
