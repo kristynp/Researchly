5a23c072c941e6fdb3d328a6ca254b236478ef35
github username <kristyparker1@gmail.com>
2020-08-26 14:08:32 -0600

    add create action for resource notes

--- app/controllers/resource_notes_controller.rb
+++ app/controllers/resource_notes_controller.rb
@@ -1,2 +1,34 @@
 class ResourceNotesController < ApplicationController
+  before_action :redirect_if_not_logged_in
+
+  def new
+    if params[:resource_id] && @resource = Resource.find_by_id(params[:resource_id]) 
+      @resource = Resource.find_by_id(params[:resource_id]) 
+      @resource_note = @resource.resource_notes.build
+    else
+      @error = "That resource does not exist" if params[:resource_id]
+      @resource_note = ReseourceNote.new
+    end 
+  end
+
+  def create
+    @resource = Resource.find_by_id(params[:resource_id])
+    if @resource  
+      @resource_note = @resource.resource_notes.build(resource_note_params)
+    else
+      @error = "Please create a new resource note from the resource page it will be added to."
+      redirect_to resources_path
+    end
+    if @resource_note.save
+      redirect_to resource_path(@resource)
+    else
+      render :new
+    end 
+  end
+
+  private 
+  
+  def resource_note_params
+    params.require(:resource_note).permit(:title, :content, :resource_id) 
+  end
 end
