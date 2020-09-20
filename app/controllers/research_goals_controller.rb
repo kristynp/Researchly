f7ad8cd96e07a547d19617c0b4b688c907f3764b
github username <kristyparker1@gmail.com>
2020-09-19 23:18:33 -0600

    refactor to set research goal before relevant actions

--- app/controllers/research_goals_controller.rb
+++ app/controllers/research_goals_controller.rb
@@ -1,4 +1,5 @@
 class ResearchGoalsController < ApplicationController 
+  before_action :set_research_goal, only: [:show, :edit, :update, :destroy]
   
   def index
     if params[:user_id] && @user = User.find_by_id(params[:user_id])
@@ -17,24 +18,20 @@ class ResearchGoalsController < ApplicationController
     @research_goal = current_user.research_goals.build(research_goal_params)
     if @research_goal.save 
       redirect_to research_goals_path
-      # new http request
     else 
       render :new 
     end 
   end
 
   def show 
-    @research_goal = ResearchGoal.find_by_id(params[:id])
     redirect_to research_goals_path if !@research_goal
   end
 
   def edit
-    @research_goal = ResearchGoal.find_by(id: params[:id])
     @current_user = current_user
   end
 
   def update
-    @research_goal = ResearchGoal.find_by(id: params[:id])
     if @research_goal.update(research_goal_params)
       redirect_to @research_goal
     else
@@ -43,7 +40,6 @@ class ResearchGoalsController < ApplicationController
   end
 
   def destroy
-    @research_goal = ResearchGoal.find_by(id: params[:id])
     @research_goal.resources.each do |resource| 
       resource.destroy
     end
@@ -53,7 +49,9 @@ class ResearchGoalsController < ApplicationController
 
   private 
 
-  #could make private method in this class to set research goal to make more DRY
+  def set_research_goal
+    @research_goal = ResearchGoal.find_by(id: params[:id])
+  end
 
   def research_goal_params
     params.require(:research_goal).permit(:name, :description, :notes) 
