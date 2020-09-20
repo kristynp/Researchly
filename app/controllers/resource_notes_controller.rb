0ce9c6e1514809d60e2886d8490f4ea25bd7fdfe
github username <kristyparker1@gmail.com>
2020-09-19 23:50:57 -0600

    refactor to set resource and resource note before relevant actions

--- app/controllers/resource_notes_controller.rb
+++ app/controllers/resource_notes_controller.rb
@@ -1,9 +1,10 @@
 class ResourceNotesController < ApplicationController
   before_action :redirect_if_not_logged_in
+  before_action :set_resource, only: [:create, :edit, :update]
+  before_action :set_resource_note, only: [:edit, :update]
 
   def new
     if params[:resource_id] && @resource = Resource.find_by_id(params[:resource_id]) 
-      @resource = Resource.find_by_id(params[:resource_id]) 
       @resource_note = @resource.resource_notes.build
     else
       @error = "That resource does not exist" if params[:resource_id]
@@ -12,12 +13,10 @@ class ResourceNotesController < ApplicationController
   end
 
   def create
-    @resource = Resource.find_by_id(params[:resource_id])
     if @resource  
       @resource_note = @resource.resource_notes.build(resource_note_params)
     else
       @error = "Please create a new resource note from the resource page it will be added to."
-      redirect_to resources_path
     end
     if @resource_note.save
       redirect_to resource_path(@resource)
@@ -26,15 +25,7 @@ class ResourceNotesController < ApplicationController
     end 
   end
 
-  def edit
-    @resource = Resource.find_by(id: params[:resource_id])
-    @resource_note = ResourceNote.find_by(id: params[:id])
-  end
-
   def update
-    @resource = Resource.find_by(id: params[:resource_id])
-    @resource_note = ResourceNote.find_by(id: params[:id])
-
     if @resource_note.update(resource_note_params)
       redirect_to @resource
     else 
@@ -43,21 +34,26 @@ class ResourceNotesController < ApplicationController
   end
 
   def destroy
-    @resource = Resource.find_by(id: params[:id]) 
-    @resource_note = ResourceNote.find_by(id: params[:format]) #why is this under format in params?
-
+    @resource = Resource.find_by(id: params[:id])
+    @resource_note = ResourceNote.find_by(id: params[:format])
     if @resource_note 
       @resource_note.destroy
     else
       @error = "Resource not found"
     end
-
-    redirect_to resource_path(@resource)
+    redirect_to resource_path(@resource) 
   end
 
-  
 
   private 
+  
+  def set_resource
+    @resource = Resource.find_by(id: params[:resource_id])
+  end
+
+  def set_resource_note
+    @resource_note = ResourceNote.find_by(id: params[:id])
+  end
   
   def resource_note_params
     params.require(:resource_note).permit(:title, :content, :resource_id) 
