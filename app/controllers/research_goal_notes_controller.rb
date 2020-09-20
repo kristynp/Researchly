01fd6e93f363da02f33aab384e2d4b36d0d9ecb1
github username <kristyparker1@gmail.com>
2020-09-19 22:59:47 -0600

    refactor with before action to set research goal note

--- app/controllers/research_goal_notes_controller.rb
+++ app/controllers/research_goal_notes_controller.rb
@@ -1,10 +1,10 @@
 class ResearchGoalNotesController < ApplicationController
   before_action :redirect_if_not_logged_in
   before_action :set_research_goal, except: [:index, :new]
+  before_action :set_research_goal_note, only: [:edit, :update, :destroy]
 
   def new
     if params[:research_goal_id] && @research_goal = ResearchGoal.find_by_id(params[:research_goal_id]) 
-      @research_goal = ResearchGoal.find_by_id(params[:research_goal_id]) 
       @research_goal_note = @research_goal.research_goal_notes.build
     else
       @error = "That research goal does not exist" if params[:research_goal_id]
@@ -16,31 +16,27 @@ class ResearchGoalNotesController < ApplicationController
     if @research_goal  
       @research_goal_note = @research_goal.research_goal_notes.build(research_goal_note_params)
     else
-      @error = "Please create a new research goal note from the research goal page it will be added to."
+      @error = "Please create a new note from the research goal page it will be associated with."
       redirect_to research_goals_path
     end
     if @research_goal_note.save
       redirect_to research_goal_path(@research_goal)
     else
+      @error = "Unable to save note. Please fill in all fields."
       render :new
     end 
   end
 
-  def edit
-    @research_goal_note = ResearchGoalNote.find_by(id: params[:id])
-  end
-
   def update
-    @research_goal_note = ResearchGoalNote.find_by(id: params[:id])
     if @research_goal_note.update(research_goal_note_params)
       redirect_to @research_goal
-    else 
+    else
+      @error = "Unable to update note."
       render :edit 
     end
   end
 
   def destroy
-    @research_goal_note = ResearchGoalNote.find_by(id: params[:id])
     if @research_goal_note 
       @research_goal_note.destroy
     else
@@ -49,10 +45,15 @@ class ResearchGoalNotesController < ApplicationController
     redirect_to research_goal_path(@research_goal)
   end
 
+
   private 
 
   def set_research_goal 
     @research_goal = ResearchGoal.find_by(id: params[:research_goal_id])
+  end
+
+  def set_research_goal_note
+    @research_goal_note = ResearchGoalNote.find_by(id: params[:id])
   end
   
   def research_goal_note_params
